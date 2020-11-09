eaea5eea35a957561b2788f1936e363b8ef13e83
Donnel <donnel206@hotmail.com>
2020-11-09 17:52:30 +0100

    test

--- Vraag4/Program.cs
+++ Vraag4/Program.cs
@@ -20,26 +20,25 @@ namespace Vraag4
             bool IsRestart = false;
             bool playGame = true;
             char[,] tabel = new char[10, 10];
-            do//this only loops again when the game is restarted
+            do
             {
-                playGame = true;//the game loop should play
-                IsRestart = false;//the game should not restart by default after a game loop
-                xPos = random.Next(tabel.GetLength(0));//new random value assigned
+                playGame = true;
+                IsRestart = false;
+                xPos = random.Next(tabel.GetLength(0));
                 yPos = random.Next(tabel.GetLength(0));
-                for (int i = 0; i < tabel.GetLength(0); i++)//table is filled with '-'
+                for (int i = 0; i < tabel.GetLength(0); i++)
                 {
                     for (int j = 0; j < tabel.GetLength(1); j++)
                     {
                         tabel[i, j] = '-';
                     }
                 }
-                do//main game loop
+                do
                 {
-                    //Console.WriteLine($"x = {xPos}");
-                    //Console.WriteLine($"y = {yPos}");
+
                     Compare(xInput, xPos, 'x');
                     Compare(yInput, yPos, 'y');
-                    for (int i = 0; i < tabel.GetLength(0); i++)//the table is shown
+                    for (int i = 0; i < tabel.GetLength(0); i++)
                     {
                         for (int j = 0; j < tabel.GetLength(1); j++)
                         {
@@ -47,19 +46,19 @@ namespace Vraag4
                         }
                         Console.WriteLine();
                     }
-                    //user input
+                  
                     Console.WriteLine("\nGeef een rij (0-9)");
                     xInput = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Geef een kolom (0-9)");
                     yInput = Convert.ToInt32(Console.ReadLine());
                     tabel[xInput, yInput] = 'x';
                     Console.Clear();
-                    if (xInput == xPos && yInput == yPos)//check if user found the egg
+                    if (xInput == xPos && yInput == yPos)
                     {
                         Console.WriteLine($"x = {xPos}");
                         Console.WriteLine($"y = {yPos}");
                         Console.WriteLine("Je hebt het ei gevonden!");
-                        Console.WriteLine("Druk <enter> om opnieuw to spelen of typ <e> om te stoppen");
+                        Console.WriteLine("Druk <enter> om opnieuw to spelen of typ e om te stoppen");
                         if (Console.ReadLine().ToLower() == "e")
                         {
                             playGame = false;
@@ -67,19 +66,19 @@ namespace Vraag4
                         }
                         else
                         {
-                            //when enter is pressed without typing anything or the character entered is not 'e' 
+                            
                             IsRestart = true;
                             playGame = false;
-                            //user input is reset
+                            
                             xInput = -1;
                             yInput = -1;
                             Console.Clear();
                         }
                     }
-                } while (playGame);//end of main game loop   
+                } while (playGame);
             } while (IsRestart);
         }
-        static void Compare(int input, int pos, char orientation)//tells the user which orientation the egg is pointed compared to the user input
+        static void Compare(int input, int pos, char orientation)
         {
             if (input != -1)
             {
@@ -87,22 +86,22 @@ namespace Vraag4
                 {
                     if (input > pos)
                     {
-                        Console.WriteLine("Het ei ligt meer in het westen.");
+                        Console.WriteLine("Het ei ligt meer naar het westen.");
                     }
                     else if (input < pos)
                     {
-                        Console.WriteLine("Het ei ligt meer in het oosten.");
+                        Console.WriteLine("Het ei ligt meer naar het oosten.");
                     }
                 }
                 else if (orientation == 'y')
                 {
                     if (input > pos)
                     {
-                        Console.WriteLine("Het ei ligt meer in het noorden.");
+                        Console.WriteLine("Het ei ligt meer naar het noorden.");
                     }
                     else if (input < pos)
                     {
-                        Console.WriteLine("Het ei ligt meer in het zuiden.");
+                        Console.WriteLine("Het ei ligt meer naar het zuiden.");
                     }
                 }
             }
